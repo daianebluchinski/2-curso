28b5fdf704ede78216118e38c496853e59962732
daianebluchinski <daiane.bluchinski@escola.pr.gov.br>
2025-06-10 07:27:34 -0300

    ok

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
-} 
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave() {}
